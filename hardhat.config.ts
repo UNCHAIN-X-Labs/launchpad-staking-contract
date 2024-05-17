656e85abd1c61a588f704a235f38ac6c4e0fcfe0
Unchain-X <unchainx.aws@gmail.com>
2024-05-17 16:25:22 +0900

    chore: set ledger config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import 'solidity-docgen';
-import { SolcConfig } from "hardhat/types";
 import * as dotenv from 'dotenv';
+import { SolcConfig } from "hardhat/types";
+import 'solidity-docgen';
+import 'hardhat-gas-reporter';
+import "@nomicfoundation/hardhat-ledger";
 
 dotenv.config();
 
@@ -36,17 +38,15 @@ const config: HardhatUserConfig = {
     bnb: {
       url: process.env.BNB_MAINNET_URL,
       // accounts: [process.env.PRIVATE_KEY || ''],
+      ledgerAccounts: [
+        "0x6a924a143476753395BdE8a0565baCccDF90Abcf",
+      ],
       chainId: 56,
     },
     bnbtest: {
       url: process.env.BNB_TESTNET_URL,
       accounts: [process.env.PRIVATE_KEY || ''],
       chainId: 97
-    },
-    arbitrum: {
-      url: process.env.ARB_MAINNET_URL,
-      // accounts: [process.env.PRIVATE_KEY || ""],
-      chainId: 42161
     },
   },
   mocha: {
