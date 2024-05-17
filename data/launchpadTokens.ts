578ca8d0987c90c6c8312048398d85e617dcd179
Unchain-X <unchainx.aws@gmail.com>
2024-05-17 16:23:01 +0900

    update token address for mainnet

--- data/launchpadTokens.ts
+++ data/launchpadTokens.ts
@@ -1,4 +1,4 @@
-export const LAUNCHPAD_TOKENS_TEST = [
+export const LAUNCHPAD_TOKENS_MAINNET = [
   {
     name: 'BNB Token',
     symbol: 'BNB',
@@ -9,50 +9,50 @@ export const LAUNCHPAD_TOKENS_TEST = [
   {
     name: 'Binance Pegged USDT',
     symbol: 'USDT',
-    address: '',
+    address: '0x55d398326f99059fF775485246999027B3197955',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0x55d398326f99059fF775485246999027B3197955.png',
   },
   {
     name: 'Dogecoin',
     symbol: 'DOGE',
-    address: '',
+    address: '0xbA2aE424d960c26247Dd6c32edC70B295c744C43',
     decimals: 8,
     logoURI: 'https://tokens.pancakeswap.finance/images/0xbA2aE424d960c26247Dd6c32edC70B295c744C43.png',
   },
   {
     name: 'Binance Pegged Bitcoin',
     symbol: 'BTCB',
-    address: '',
+    address: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c.png',
   },
   {
     name: 'First Digital USD',
     symbol: 'FDUSD',
-    address: '',
+    address: '0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409.png',
   },
   {
     name: 'Binance Pegged ETH',
     symbol: 'ETH',
-    address: '',
+    address: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0x2170Ed0880ac9A755fd29B2688956BD959F933F8.png',
   },
   {
     name: 'SOLANA',
     symbol: 'SOL',
-    address: '',
+    address: '0x570A5D26f7765Ecb712C0924E4De545B89fD43dF',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0x570A5D26f7765Ecb712C0924E4De545B89fD43dF.png',
   },
   {
     name: 'XRP Token',
     symbol: 'XRP',
-    address: '',
+    address: '0x1D2F0da169ceB9fC7B3144628dB156f3F6c60dBE',
     decimals: 18,
     logoURI: 'https://tokens.pancakeswap.finance/images/0x1D2F0da169ceB9fC7B3144628dB156f3F6c60dBE.png',
   }
-]
+];
