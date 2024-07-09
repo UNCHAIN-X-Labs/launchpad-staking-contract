05ca62638bd87c0ae7434941d3c4884c16e9aeac
Unchain-X <unchainx.aws@gmail.com>
2024-07-09 17:20:07 +0900

    update script for UNXFaucet

--- scripts/faucet/faucet-set.ts
+++ scripts/faucet/faucet-set.ts
@@ -1,18 +1,15 @@
-import { parseEther } from "ethers";
 import { ethers } from "hardhat";
-import { MINING_MULTIPLIER } from "../data/miningMultiplier";
-import { LAUNCHPAD_TOKENS_MAINNET } from "../data/launchpadTokens";
 
 async function main() {
     const params = {
       countLimit: 10,
       startTimestamp: 1723680000,  // 2024-08-15 00:00:00 UTC
-      cycle: 2592000               // 30 Days
+      cycle: 2592000               // 30 Days (86400 * 30)
     }
-    const rewardClaim = await ethers.getContractAt("RewardClaim", "");
+    const faucet = await ethers.getContractAt('UNXFaucet', "");
 
     console.log(`Setting claim schedule..`);
-    const setTx = await rewardClaim.setClaimSchedule(params)
+    const setTx = await faucet.setClaimSchedule(params)
       .then(async (res) => {
           const receipt = await res.wait();
           if(receipt?.status == 1) {
